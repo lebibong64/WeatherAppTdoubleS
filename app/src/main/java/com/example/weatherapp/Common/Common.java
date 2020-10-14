a83a22ef8ceda675ef28d5ca03bc6fc615dce9d0
lebibong64 <lebibong64@gmail.com>
2020-10-14 12:40:27 +0700

    HI

---
+++ app/src/main/java/com/example/weatherapp/Common/Common.java
@@ -0,0 +1,9 @@
+package com.example.weatherapp.Common;
+
+import android.location.Location;
+
+public class Common {
+
+    public static final String APP_ID = "25152a159a39d587ed862311e34e253f";
+    public static Location current_location=null;
+}
